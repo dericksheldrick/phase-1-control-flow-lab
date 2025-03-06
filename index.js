51409ea5ba8992ba1b33f62548fdbb4a903b5a9d
Derick Sheldrick <sheldrickderick9@gmail.com>
2025-03-06 13:20:39 +0300

    control flow submission

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  
+  if (ride <= 400){
+    console.log('This one is on me!');
+    return ('This one is on me!');
+  } else if(ride <= 2000){
+    console.log('That will be twenty bucks.');
+    return ('That will be twenty bucks.');
+  } else if (ride <= 2500){
+    console.log('I will gladly take your thirty bucks.');
+    return ('I will gladly take your thirty bucks.');
+  } else {
+    return ('No can do.')
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  // Write your code h ere!
+  return city ==='NYC' ? "Ok, sounds good.":"No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+
+  switch (tip) {
+    case 'generous':
+      return ("Thank you so much.");
+
+    case 'not as generous':
+      return ('Thank you.');
+  
+    default:
+      return ('Bye.');
+  }
 }
